fd00b313d253425298ba9a207a7e226de9af7e10
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-16 20:53:07 +0100

    feat: added letter navigation

--- examples/redis/src/pages/[[...letter]].tsx
+++ examples/redis/src/pages/[[...letter]].tsx
@@ -1,26 +1,44 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { alphabet } from '~/lib/alphabet'
 
-const HomePage = () => (
-  <div>
-    <h1>Cache Tags Alphabet</h1>
+const usePageLetter = () => useRouter().query.letter?.[0].toUpperCase() ?? null
 
-    <p>
-      Every letter has a page. Every page depends on the letter, and it's
-      sibling letters. <br />
-      <strong>
-        Cmd+click a letter to renew the cache of all related letter pages.
-      </strong>
-    </p>
+const HomePage = () => {
+  const current = usePageLetter()
 
-    <ul id="alphabet">
-      {alphabet.map(letter => (
-        <li key={letter} tabIndex={1}>
-          <Link href={`/${letter}`}>{letter}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-)
+  return (
+    <div>
+      <h1>Cache Tags Alphabet</h1>
+
+      <p>
+        Every letter has a page. Every page depends on the letter, and it's
+        sibling letters.
+      </p>
+
+      <p>
+        <strong>
+          1) Click a letter to navigate to it's page (shows underlined)
+        </strong>
+        <br />
+        <strong>
+          2) Cmd+click a letter to renew the cache of all related letter pages.
+        </strong>
+      </p>
+
+      <ul id="alphabet">
+        {alphabet.map(letter => (
+          <li
+            key={letter}
+            tabIndex={1}
+            className={current === letter ? 'current' : ''}
+          >
+            <Link href={`/${letter}`}>{letter}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default HomePage
