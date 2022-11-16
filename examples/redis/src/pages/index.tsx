4322479335292f043600f9329620417b58b0eb1e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-16 20:37:44 +0100

    feat: alphabet page

--- examples/redis/src/pages/index.tsx
+++ examples/redis/src/pages/index.tsx
@@ -1,3 +1,23 @@
-const HomePage = () => <div>Home page</div>
+import { alphabet } from '~/lib/alphabet'
+
+const HomePage = () => (
+  <div>
+    <h1>Cache Tags Alphabet</h1>
+
+    <p>
+      Every letter represents a cached page. Every page depends on the letter,
+      and it's sibling letters. <br />
+      <strong>Click a letter to renew the cache of all related letters.</strong>
+    </p>
+
+    <ul id="alphabet">
+      {alphabet.map(letter => (
+        <li key={letter} tabIndex={1}>
+          {letter}
+        </li>
+      ))}
+    </ul>
+  </div>
+)
 
 export default HomePage
