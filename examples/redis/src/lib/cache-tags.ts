fc809a4b9c45569e85798ddbd25f2faa252a899b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-17 22:33:33 +0100

    feat: added cache-tags config

---
+++ examples/redis/src/lib/cache-tags.ts
@@ -0,0 +1,21 @@
+import {
+  RedisCacheTagsRegistry,
+  CacheTags,
+  defaultGenerateHash,
+} from 'next-cache-tags'
+
+if (!process.env.CACHE_TAGS_REDIS_URL) {
+  throw new Error('Missing CACHE_TAGS_REDIS_URL environment variable')
+}
+
+const debugging = Boolean(process.env.CACHE_TAGS_DEBUG)
+
+const cacheTags = new CacheTags({
+  registry: new RedisCacheTagsRegistry({
+    url: process.env.CACHE_TAGS_REDIS_URL,
+  }),
+  generateHash: debugging ? false : defaultGenerateHash,
+  log: debugging,
+})
+
+export { cacheTags }
