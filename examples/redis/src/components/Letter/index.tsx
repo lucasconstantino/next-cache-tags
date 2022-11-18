f40965eb5a2643ae2dccb531872edfd6e2846b29
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-18 19:07:52 +0100

    feat: added colouring based on age

--- examples/redis/src/components/Letter/index.tsx
+++ examples/redis/src/components/Letter/index.tsx
@@ -18,8 +18,26 @@ type TProps = {
  */
 const invalidate = (letter: TLetter) => navigator.sendBeacon(`/api/invalidate?tag=letter:${letter}`)
 
+const maxAge = 60 // 1 minute
+
+/**
+ * Resolve a color based on cache age, from green (recent) to red (old).
+ */
+const getColor = (age: number | null) => {
+  if (!age) {
+    return 'black'
+  }
+
+  const perc = Math.max(0, Math.min(1, age / maxAge))
+
+  // RGB construction based on age percentage.
+  return `rgb(${perc * 255}, ${255 - perc * 255}, 0)`
+}
+
 const Letter: React.FC<TProps> = ({ letter, isCurrent, isPrevious, isNext, cache }) => {
   const age = useCacheAge(cache)
+  const color = getColor(age)
+  const style = { '--time-color': color } as React.CSSProperties
 
   return (
     <Link
@@ -27,6 +45,7 @@ const Letter: React.FC<TProps> = ({ letter, isCurrent, isPrevious, isNext, cache
       href={`/alphabet/${isCurrent ? '' : letter}`}
       // Invalidate (instead of navigate) if cmd/ctrl is pressed upon clicking.
       onClick={(e: React.MouseEvent) => e.metaKey && (e.preventDefault(), invalidate(letter))}
+      style={style}
       className={classnames('letter', {
         current: isCurrent,
         highlighted: isCurrent || isPrevious || isNext,
