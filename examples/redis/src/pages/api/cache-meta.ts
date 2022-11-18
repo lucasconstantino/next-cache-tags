b832ac3da7dba4f3fda4a37a8a6b7de6d0dd1ad6
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-18 02:10:05 +0100

    feat: added /api/cache-meta reader

---
+++ examples/redis/src/pages/api/cache-meta.ts
@@ -0,0 +1,31 @@
+import type { NextApiHandler } from 'next'
+import { cacheTags } from '~/lib/cache-tags'
+
+/**
+ * Load cache-tags and time maps from Redis.
+ */
+const handler: NextApiHandler = async (_req, res) =>
+  await cacheTags.registry.act(async (client) => {
+    const tags = await client.KEYS('*')
+    const paths: Record<string, string> = {}
+    const transaction = client.multi()
+
+    for (const tag of tags) {
+      transaction.HGETALL(tag)
+    }
+
+    const result = (await transaction.exec()) as unknown[] as Record<
+      string,
+      string
+    >[]
+
+    for (const hashes of result) {
+      for (const path in hashes) {
+        paths[path] = paths[path] > hashes[path] ? paths[path] : hashes[path]
+      }
+    }
+
+    res.json(paths)
+  })
+
+export default handler
