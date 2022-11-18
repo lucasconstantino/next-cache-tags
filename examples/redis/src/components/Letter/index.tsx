972a16952c866b11cf5af7ffd4f37970066d5187
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-18 20:01:52 +0100

    feat: moved useAgeColor to own hook

--- examples/redis/src/components/Letter/index.tsx
+++ examples/redis/src/components/Letter/index.tsx
@@ -4,6 +4,7 @@ import classnames from 'classnames'
 import type { TLetter } from '~/lib/alphabet'
 import { memo } from 'react'
 import { useCacheAge } from '~/hooks/useCacheAge'
+import { useAgeColor } from '~/hooks/useAgeColor'
 
 type TProps = {
   letter: TLetter
@@ -18,25 +19,9 @@ type TProps = {
  */
 const invalidate = (letter: TLetter) => navigator.sendBeacon(`/api/invalidate?tag=letter:${letter}`)
 
-const maxAge = 60 // 1 minute
-
-/**
- * Resolve a color based on cache age, from green (recent) to red (old).
- */
-const getColor = (age: number | null) => {
-  if (age === null) {
-    return 'black'
-  }
-
-  const perc = Math.max(0, Math.min(1, age / maxAge))
-
-  // RGB construction based on age percentage.
-  return `rgb(${perc * 255}, ${255 - perc * 255}, 0)`
-}
-
 const Letter: React.FC<TProps> = ({ letter, isCurrent, isPrevious, isNext, cache }) => {
   const age = useCacheAge(cache)
-  const color = getColor(age)
+  const color = useAgeColor(age)
   const style = { '--time-color': color } as React.CSSProperties
 
   return (
---
+++ examples/redis/src/hooks/useAgeColor.ts
@@ -0,0 +1,15 @@
+/**
+ * Resolve a color based on cache age, from green (recent) to red (old).
+ */
+const useAgeColor = (age: number | null, maxAge = 60) => {
+  if (age === null) {
+    return 'black'
+  }
+
+  const perc = Math.max(0, Math.min(1, age / maxAge))
+
+  // RGB construction based on age percentage.
+  return `rgb(${perc * 255}, ${255 - perc * 255}, 0)`
+}
+
+export { useAgeColor }
