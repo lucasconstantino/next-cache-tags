951bdbdd6ddf38594c9c8dd29b8d63663d673c8a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-20 17:19:39 +0100

    feat: added all cache to status

--- examples/redis/src/components/CacheStatus/index.tsx
+++ examples/redis/src/components/CacheStatus/index.tsx
@@ -1,34 +1,31 @@
 import { useAgeColor } from '~/hooks/useAgeColor'
 import { useCacheAge } from '~/hooks/useCacheAge'
 import type { TCache } from '~/hooks/useCacheMeta'
+import { alphabet } from '~/lib/alphabet'
 
-const CacheItem: React.FC<{ cacheKey: string; date: Date }> = ({ cacheKey, date }) => {
+const CacheItem: React.FC<{ cacheKey: string; date?: Date }> = ({ cacheKey, date }) => {
   const age = useCacheAge(date)
   const color = useAgeColor(age)
-  const style = { '--time-color': color } as React.CSSProperties
+  const style = { '--time-color': color, opacity: date ? 1 : 0.25 } as React.CSSProperties
 
   return (
     <div className="cache-status-item" style={style}>
-      <strong>{cacheKey}:</strong> {date.toISOString()}
+      <strong>{cacheKey}:</strong> {date?.toISOString() ?? 'empty'}
     </div>
   )
 }
 
-const CacheStatus: React.FC<{ cache: TCache }> = ({ cache }) => {
-  const pairs = Object.entries(cache).sort(([a], [b]) => (a < b ? -1 : a > b ? 1 : 0))
-
-  return (
-    <section id="cache-status">
-      <h3>Cache status</h3>
-      <ul>
-        {pairs.map(([cacheKey, date]) => (
-          <li key={cacheKey}>
-            <CacheItem cacheKey={cacheKey} date={date} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  )
-}
+const CacheStatus: React.FC<{ cache: TCache }> = ({ cache }) => (
+  <section id="cache-status">
+    <h3>Cache status</h3>
+    <ul>
+      {alphabet.map((letter) => (
+        <li key={letter}>
+          <CacheItem cacheKey={letter} date={cache[`/alphabet/${letter}`]} />
+        </li>
+      ))}
+    </ul>
+  </section>
+)
 
 export { CacheStatus }
