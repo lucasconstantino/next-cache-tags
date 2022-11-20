843a735806c677be870ed0eca4e2335a2f3390b2
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-21 00:49:30 +0100

    feat: disabled prefetching

--- examples/redis/src/pages/_app.tsx
+++ examples/redis/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  // Disable any sorts of prefetching
+  router.prefetch = async () => void 0
+
   return <Component {...pageProps} />
 }
 export default MyApp
