9c2650bb23955e48a97dadd215a24a9bd68b7bd4
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-21 00:48:23 +0100

    feat: added invalidation logic to useCacheInfo

--- examples/redis/src/hooks/useCacheInfo.ts
+++ examples/redis/src/hooks/useCacheInfo.ts
@@ -2,6 +2,7 @@ import { useCallback, useMemo, useState } from 'react'
 import type { TLetter } from '../lib/alphabet'
 
 type TCacheKey = `/alphabet/${TLetter}`
+type TCacheTag = `letter:${string}`
 type TCacheMap = { [key in TCacheKey]?: Date }
 
 const parse = {
@@ -18,12 +19,20 @@ const parse = {
 }
 
 /**
- * Cache information state resolver.
+ * Resolves a cache-tag given a letter.
+ */
+const createCacheTag = (letter: TLetter | null): TCacheTag => `letter:${letter}`
+
+/**
+ * Cache status and invalidation connector.
  */
 const useCacheInfo = () => {
   const [cache, setCache] = useState<TCacheMap>({})
   const [updating, setUpdating] = useState(false)
 
+  /**
+   * Performs a cache info update.
+   */
   const update = useCallback(async () => {
     setUpdating(true)
 
@@ -33,13 +42,28 @@ const useCacheInfo = () => {
       .then(setCache)
 
     setUpdating(false)
-  }, [setCache])
+  }, [setUpdating, setCache])
 
-  return useMemo(() => ({ cache, update, updating }), [cache, update, updating])
+  /**
+   * Performs an invalidation of a letter cache-tag.
+   */
+  const invalidate = useCallback(
+    async (letter: TLetter) => {
+      setUpdating(true)
+      await fetch(`/api/invalidate?tag=${createCacheTag(letter)}`)
+      await update()
+    },
+    [setUpdating, update]
+  )
+
+  return useMemo(
+    () => ({ cache, update, updating, invalidate }),
+    [cache, update, updating, invalidate]
+  )
 }
 
 type TCacheInfo = ReturnType<typeof useCacheInfo>
 
 export type { TCacheKey, TCacheMap, TCacheInfo }
 
-export { useCacheInfo }
+export { useCacheInfo, createCacheTag }
