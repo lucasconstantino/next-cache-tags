42fe2fea759f6678cc16550381b5c50a21d8a31d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-22 01:19:47 +0100

    feat: avoid redis socket timeouts

--- examples/redis/src/lib/cache-tags.ts
+++ examples/redis/src/lib/cache-tags.ts
@@ -1,8 +1,4 @@
-import {
-  RedisCacheTagsRegistry,
-  CacheTags,
-  defaultGenerateHash,
-} from 'next-cache-tags'
+import { RedisCacheTagsRegistry, CacheTags, defaultGenerateHash } from 'next-cache-tags'
 
 if (!process.env.CACHE_TAGS_REDIS_URL) {
   throw new Error('Missing CACHE_TAGS_REDIS_URL environment variable')
@@ -13,6 +9,7 @@ const debugging = Boolean(process.env.CACHE_TAGS_DEBUG)
 const cacheTags = new CacheTags({
   registry: new RedisCacheTagsRegistry({
     url: process.env.CACHE_TAGS_REDIS_URL,
+    socket: { connectTimeout: 50000 },
   }),
   generateHash: debugging ? false : defaultGenerateHash,
   log: debugging,
