7fb51ef2ab01f11d21353c9409da531f413d1ad7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2022-11-26 14:56:46 +0100

    test: test edge cases, and configs on cache-tags class

--- src/lib/cache-tags.test.ts
+++ src/lib/cache-tags.test.ts
@@ -1,20 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { createMocks } from 'node-mocks-http'
 import { CacheTags } from './cache-tags'
 import type { TagsResolver } from './cache-tags'
-import { MemoryCacheTagsRegistry } from './registry/memory'
+import { CacheTagsRegistry } from './registry/base'
+
+/**
+ * Creates a mocked registry.
+ */
+const getRegistryMock = (): CacheTagsRegistry => {
+  const registry: { [key: string]: string[] } = {}
+
+  return {
+    register: jest.fn((path, tags) => {
+      for (const tag of tags) {
+        registry[tag] = (registry[tag] ?? []).concat(path)
+      }
+    }),
+
+    extract: jest.fn((tag) => {
+      const paths = registry[tag] ?? []
+      delete registry[tag]
+      return paths
+    }),
+  }
+}
+
+/**
+ * Create network mock.
+ */
+const getNetworkMock = (...params: Parameters<typeof createMocks>) => {
+  const { req, res } = createMocks<NextApiRequest, NextApiResponse>(...params)
+
+  res.revalidate = jest.fn()
+
+  return { req, res }
+}
 
 describe('CacheTags', () => {
-  let registry: MemoryCacheTagsRegistry
+  let registry: CacheTagsRegistry
 
-  beforeEach(() => (registry = new MemoryCacheTagsRegistry()))
+  beforeEach(() => (registry = getRegistryMock()))
 
   it('should be possible to create an instance of CacheTags', () => {
     expect(() => new CacheTags({ registry })).not.toThrow()
     expect(new CacheTags({ registry })).toBeInstanceOf(CacheTags)
   })
 
-  describe('hash generator', () => {
-    it('should be possible to use the hash generator', () => {
+  describe('::generateHash', () => {
+    it('should be possible to use the default hash generator', () => {
       const cacheTags = new CacheTags({ registry })
       expect(typeof cacheTags.generateHash('some-id')).toBe('string')
     })
@@ -23,9 +56,33 @@ describe('CacheTags', () => {
       const cacheTags = new CacheTags({ registry, generateHash: () => 'tag' })
       expect(cacheTags.generateHash('some-id')).toBe('tag')
     })
+
+    it('should be possible to disable hash generator', () => {
+      const cacheTags = new CacheTags({ registry, generateHash: false })
+      expect(cacheTags.generateHash('some-id')).toBe('some-id')
+    })
   })
 
-  describe('invalidator', () => {
+  describe('::register', () => {
+    it('should register a cache tags for a path', () => {
+      const cacheTags = new CacheTags({ registry, generateHash: false })
+      cacheTags.register('/some-path', ['tag-1'])
+
+      expect(registry.register).toHaveBeenCalledWith('/some-path', ['tag-1'])
+    })
+
+    it('should hash registering cache', () => {
+      const generateHash = jest.fn(() => 'hashed')
+      const cacheTags = new CacheTags({ registry, generateHash })
+
+      cacheTags.register('/some-path', ['tag-1'])
+
+      expect(generateHash).toHaveBeenCalledWith('tag-1')
+      expect(registry.register).toHaveBeenCalledWith('/some-path', ['hashed'])
+    })
+  })
+
+  describe('::invalidator', () => {
     const resolver: TagsResolver = jest.fn((req) => [req.query.tag as string])
 
     it('should create a handler function', () => {
@@ -35,7 +92,7 @@ describe('CacheTags', () => {
       expect(invalidator).toBeInstanceOf(Function)
     })
 
-    it('should execute tags resolver upon invokation', async () => {
+    it('should use tags resolver', async () => {
       const resolver: TagsResolver = jest.fn(() => [])
       const cacheTags = new CacheTags({ registry })
       const invalidator = cacheTags.invalidator({ resolver })
@@ -46,46 +103,104 @@ describe('CacheTags', () => {
       expect(resolver).toHaveBeenCalledWith(req, res)
     })
 
-    it('should execute hash generator for resolved tags', async () => {
-      const resolver = () => ['some-tag']
-      const generateHash = jest.fn(() => 'some-hash')
+    it('should use hash generator for resolved tags', async () => {
+      const resolver = () => ['tag-1', 'tag-2']
+      const generateHash = jest.fn(() => 'hash-1')
       const cacheTags = new CacheTags({ registry, generateHash })
       const invalidator = cacheTags.invalidator({ resolver })
 
-      const { req, res } = createMocks()
+      const { req, res } = getNetworkMock()
       await invalidator(req, res)
 
-      expect(generateHash).toHaveBeenCalledWith('some-tag')
-    })
-
-    it('should execute hash generator for all resolved tags', async () => {
-      const resolver = () => ['first', 'second']
-      const generateHash = jest.fn(() => 'some-hash')
-      const cacheTags = new CacheTags({ registry, generateHash })
-      const invalidator = cacheTags.invalidator({ resolver })
-
-      const { req, res } = createMocks()
-      await invalidator(req, res)
-
-      expect(generateHash).toHaveBeenCalledWith('first')
-      expect(generateHash).toHaveBeenCalledWith('second')
+      expect(generateHash).toHaveBeenCalledWith('tag-1')
+      expect(generateHash).toHaveBeenCalledWith('tag-2')
     })
 
     it('should execute revalidation for related paths', async () => {
       const cacheTags = new CacheTags({ registry, generateHash: false })
       const invalidator = cacheTags.invalidator({ resolver })
 
-      cacheTags.registry.register('/some-path', ['tag-1', 'tag-2'])
-      cacheTags.registry.register('/other-path', ['tag-1'])
-      cacheTags.registry.register('/unaffected-path', ['tag-3'])
+      cacheTags.register('/some-path', ['tag-1', 'tag-2'])
+      cacheTags.register('/other-path', ['tag-1'])
+      cacheTags.register('/unaffected-path', ['tag-3'])
 
-      const { req, res } = createMocks({ query: { tag: 'tag-1' } })
+      const { req, res } = getNetworkMock({ query: { tag: 'tag-1' } })
 
-      res.revalidate = jest.fn(res.revalidate)
       await invalidator(req, res)
 
       expect(res.revalidate).toHaveBeenCalledWith('/some-path')
       expect(res.revalidate).toHaveBeenCalledWith('/other-path')
+    })
+
+    it('should execute onSuccess callback with invalidating tags', async () => {
+      const onSuccess = jest.fn()
+      const cacheTags = new CacheTags({ registry, generateHash: false })
+      const invalidator = cacheTags.invalidator({ resolver, onSuccess })
+
+      cacheTags.register('/some-path', ['tag-1', 'tag-2'])
+
+      const { req, res } = getNetworkMock({ query: { tag: 'tag-1' } })
+
+      await invalidator(req, res)
+
+      expect(onSuccess).toHaveBeenCalledWith(req, res, ['tag-1'])
+    })
+
+    it('should execute onError callback upon invalidating error', async () => {
+      const onError = jest.fn()
+      const cacheTags = new CacheTags({
+        registry: {
+          ...registry,
+          extract: () => {
+            throw new Error('Simulated error')
+          },
+        },
+        generateHash: false,
+      })
+
+      const invalidator = cacheTags.invalidator({ resolver, onError })
+
+      cacheTags.register('/some-path', ['tag-1', 'tag-2'])
+      cacheTags.register('/other-path', ['tag-1'])
+
+      const { req, res } = getNetworkMock({ query: { tag: 'tag-1' } })
+
+      await invalidator(req, res)
+
+      expect(onError).toHaveBeenCalledWith(
+        expect.any(Error),
+        req,
+        res,
+        undefined
+      )
+    })
+
+    it('should send default successful response', async () => {
+      const cacheTags = new CacheTags({ registry })
+      const invalidator = cacheTags.invalidator({ resolver })
+
+      const { req, res } = getNetworkMock({ query: { tag: 'tag-1' } })
+      res.send = jest.fn(res.send)
+
+      await invalidator(req, res)
+
+      expect(res.send).toHaveBeenCalledWith('ok')
+    })
+
+    it('should send default error response', async () => {
+      const extract = () => Promise.reject(new Error('Simulated error'))
+      const cacheTags = new CacheTags({ registry: { ...registry, extract } })
+      const invalidator = cacheTags.invalidator({ resolver })
+
+      const { req, res } = getNetworkMock({ query: { tag: 'tag-1' } })
+
+      res.status = jest.fn(res.status)
+      res.json = jest.fn(res.json)
+
+      await invalidator(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(500)
+      expect(res.json).toHaveBeenCalledWith({ message: 'Simulated error' })
     })
   })
 })
