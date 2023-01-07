eba724d841f54324df108e8865f3dd1ffda885eb
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2023-01-07 21:59:25 +0100

    feat: added analytics

--- examples/redis/src/pages/_app.tsx
+++ examples/redis/src/pages/_app.tsx
@@ -1,13 +1,21 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { Analytics } from '@vercel/analytics/react'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
-  // Disable any sorts of prefetching
+  // Disable any sorts of prefetching to simplify comprehension
+  // of the cache-tags cleaning events.
   router.prefetch = async () => void 0
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 export default MyApp
